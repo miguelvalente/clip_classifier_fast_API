8e3b8a7c3c6dca47ee3e341d527b6dae0837f8cd
mvalente <miguelvalente@protonmail.com>
2023-02-08 19:13:02 +0100

    removed schemas

--- clip_classifier/app/app.py
+++ clip_classifier/app/app.py
@@ -1,9 +1,7 @@
-from fastapi import FastAPI, HTTPException, File, UploadFile, Query
+from fastapi import FastAPI, HTTPException, File, UploadFile
 from utils.utils import load_image, expand_list
-from schemas.image_schema import Img, ImgUrl, ImgBytes
 from pred.classifier import predict
 from fastapi.middleware.cors import CORSMiddleware
-from schemas.labels_schema import Labels
 
 
 app = FastAPI(title="Image Classifier API")
@@ -29,18 +27,6 @@ async def read_main():
     return {"msg": "Hello World !!!!"}
 
 
-@app.post("/classify/", status_code=200)
-async def classify(request: ImgUrl, labels: Labels):
-    image = load_image(request.img)
-    prediction = predict(image, labels)
-    if not prediction:
-        # the exception is raised, not returned - you will get a validation
-        # error otherwise.
-        raise HTTPException(status_code=404, detail="Image could not be downloaded")
-
-    return prediction
-
-
 @app.post("/classify_file")
 async def classify_file(labels: list[str], image: UploadFile = File(...)):
     image = load_image(await image.read())
@@ -55,30 +41,3 @@ async def classify_file(labels: list[str], image: UploadFile = File(...)):
     return prediction
 
     return {"file_size": image.file_size}
-
-
-# @app.post("/classify_file")
-# def classify_file(labels: Labels, file:  bytes = File(...)):
-#     try:
-#         contents = file.file.read()
-#         with open(file.filename, 'wb') as f:
-#             f.write(contents)
-#     except Exception:
-#         return {"message": "There was an error uploading the file"}
-#     finally:
-#         file.file.close()
-
-#     return {"message": f"Successfully uploaded {file.filename}"} 
-
-
-# @app.post("/classify_file/", status_code=200)
-# async def classify_file(labels: Labels, file: UploadFile = File(...)):
-#     request_object_content = await file.read()
-#     image = load_image(request_object_content)
-#     prediction = predict(image, labels.labels)
-#     if not prediction:
-#         # the exception is raised, not returned - you will get a validation
-#         # error otherwise.
-#         raise HTTPException(status_code=404, detail="Image could not be downloaded")
-
-    # return prediction
--- clip_classifier/schemas/labels_schema.py
+++
@@ -1,7 +0,0 @@
-from pydantic import BaseModel
-from fastapi import Query
-from typing import List
-
-
-class Labels(BaseModel):
-    labels: List[str] = Query([])
